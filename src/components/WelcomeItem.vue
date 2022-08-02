319951d4a3b3ea2134083e08ca4fbce6934b0238
mateusz.rozalski <rozalski.mkr@gmail.com>
2022-08-02 23:06:26 +0200

    Price as .title Fuel as .tag

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -2,35 +2,42 @@
   <section class="block">
     <div class="card">
 
-      <header class="card-header">
+      <header class="card-header is-flex">
+        <p class="card-header-title">
+          <slot name="heading"></slot>
+        </p>
         <p class="card-header-title">
           <slot name="heading"></slot>
         </p>
 
-        <button class="card-header-icon" aria-label="more options">
-
-        </button>
       </header>
       <div class="card-content">
         <div class="content columns">
           <div class="column">
-            <h5>Diesel</h5>
-            <h3>7,24</h3>
+            <h1 class="title">7,23</h1>
+            <span class="tag is-black">Diesel</span>
           </div>
           <div class="column">
-            <h5>98</h5>
-            <h3>6,72</h3>
+            <h1 class="title">8,23</h1>
+            <span class="tag is-dark">Diesel+</span>
           </div>
           <div class="column">
-            <h5>95</h5>
-            <h3>6,51</h3>
+            <h1 class="title">6,91</h1>
+            <span class="tag is-primary">98</span>
+          </div>
+          <div class="column">
+            <h1 class="title">6,91</h1>
+            <span class="tag is-info">98</span>
+          </div>
+          <div class="column">
+            <h1 class="title">6,91</h1>
+            <span class="tag">LPG</span>
           </div>
           <br>
         </div>
       </div>
       <footer class="card-footer">
         <a href="#" class="card-footer-item">Save</a>
-        <a href="#" class="card-footer-item">Edit</a>
         <a href="#" class="card-footer-item">Delete</a>
       </footer>
     </div>
