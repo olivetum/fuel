13fbd1427ceae7fbaf7d63a42b9d5a3e0adf021c
mateusz.rozalski <rozalski.mkr@gmail.com>
2022-08-04 20:36:28 +0200

    New Fuel card layout

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -4,11 +4,29 @@
 
 
       <div class="card-content">
-        <slot class="subtitle" name="heading"></slot>
-        <h1 class="title mr-4">
-          <slot name="dieselPrice"></slot> <sup>PLN</sup>
-        </h1>
-        <span class="tag is-black">Diesel</span>
+
+        <h4 class="has-text-weight-bold has-text-grey">
+          <slot name="heading"></slot> (4.3KM)
+        </h4>
+
+        <h2 class="title is-size-2 mt-3 mb-2">
+          <slot name="diesel"></slot>
+          <sup class="is-size-5">PLN</sup>
+        </h2>
+
+        <span class="tag is-medium is-black">Diesel</span>
+
+        <div class="mt-4 is-flex flex">
+          <div class="tag mr-1 is-danger">
+            Diesel+ : <slot name=dieselPlus></slot>
+          </div>
+          <div class="tag mr-1 is-primary">
+            Petrol : <slot name="petrol"></slot>
+          </div>
+          <div class="tag is-info">
+            Petrol+ : <slot name="petrolPlus"></slot>
+          </div>
+        </div>
       </div>
 
     </div>
